a8e682e51a2fc9b60624db2dcac4e96814605e60
lty <15936061620@163.com>
2020-05-17 15:20:24 +0800

    业务层搜索API

--- App/HttpController/Api/Search.php
+++ App/HttpController/Api/Search.php
@@ -18,8 +18,40 @@ class Search extends Base
             return $this->writeJson(Status::CODE_OK, "OK", $this->getPagingDatas(0, []));
         }
 
-        $esObj = new EsVideo();
-        $result = $esObj->searchByName($keyword, $this->params['from'], $this->params['size']);
-        return $this->writeJson(Status::CODE_OK, "OK",$result);
+        try {
+            $esObj = new EsVideo();
+            $result = $esObj->searchByName($keyword, $this->params['from'], $this->params['size']);
+        } catch (\Exception $e) {
+            // TODO
+        }
+
+        if (empty($result)) {
+            return $this->writeJson(Status::CODE_OK, "OK", $this->getPagingDatas(0,[]));
+        }
+
+        $hits = $result['hits']['hits'];
+        $total = $result['hits']['total'];
+
+        if (empty($total)) {
+            return $this->writeJson(Status::CODE_OK, "OK", $this->getPagingDatas(0,[]));
+        }
+
+        foreach ($hits as $hit) {
+            $source = $hit['_source'];
+            $resData[] = [
+                'id' => $hit['_id'],
+                'name' => $source['name'],
+                'image' => $source['image'],
+                'url' => $source['url'],
+                'type' => $source['type'],
+                'uploader' => $source['uploader'],
+                'status' => $source['status'],
+                'video_id' => $source['video_id'],
+                'create_time' => '',
+                'video_duration' => '',
+                'keywords' => [$keyword],
+            ];
+        }
+        return $this->writeJson(Status::CODE_OK, "OK", $this->getPagingDatas($total,$resData));
     }
 }
